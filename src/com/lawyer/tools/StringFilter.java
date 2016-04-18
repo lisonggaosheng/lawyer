c40d2a7b13022e6b509181fc596db4c417b3f93a
Administrator <Administrator@ZGC-20131202AJC>
2016-04-18 22:29:07 +0800

    字符串过滤

---
+++ src/com/lawyer/tools/StringFilter.java
@@ -0,0 +1,19 @@
+package com.lawyer.tools;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class StringFilter {
+	
+	public static String stringFilter(String str) throws PatternSyntaxException {
+		// 只允许字母和数字
+		// String regEx = "[^a-zA-Z0-9]";
+		// 清除掉所有特殊字符
+		String regEx = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+		Pattern p = Pattern.compile(regEx);
+		Matcher m = p.matcher(str);
+		return m.replaceAll("").trim();
+	}
+	
+}
